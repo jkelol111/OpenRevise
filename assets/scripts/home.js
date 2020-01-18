f30184423c124abda105717d2cb7824256978508
jkelol111 <jkelol111@criptext.com>
2020-01-18 22:44:41 +0800

    Fixed a stupid bug and redesigned homepage

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,5 +1,1 @@
-let UserAgent = new UAParser();
-let currentDeviceType = UserAgent.getDevice().type;
-if(currentDeviceType == "mobile") {
-  document.querySelectorAll(".home-bg")[0].src = "";
-}
+"use strict"
