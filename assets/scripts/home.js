1c3fbf8321fb1c7e60b5a2d27fd976c9aa27bbbf
Nguyen Dev <vietbetatester@outlook.com>
2020-01-20 21:38:41 +0700

    added hamburger menu

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,1 +1,11 @@
 "use strict"
+
+document.querySelector(".global-menubar-hamburger").addEventListener("click", () => {
+  document.querySelector(".global-hamburger").classList.remove("global-hamburger-state-hidden");
+  document.querySelector(".global-hamburger").classList.add("global-hamburger-state-visible");
+});
+
+document.querySelector(".global-hamburger-close").addEventListener("click", () => {
+  document.querySelector(".global-hamburger").classList.remove("global-hamburger-state-visible");
+  document.querySelector(".global-hamburger").classList.add("global-hamburger-state-hidden");
+});
---
+++ assets/scripts/menubar.js
@@ -0,0 +1,8 @@
+// "use strict"
+
+// const hostProto = window.location.protocol;
+// const hostURL = window.location.host;
+
+// document.querySelector(".global-menubar-logo").addEventListener("click", () => {
+//   window.location.assign(hostProto+"//"+hostURL+"/index.html");
+// })
