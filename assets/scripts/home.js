dc94f90b9fa032c0aa5adf43beca16dd66158418
Nguyen Dev <vietbetatester@outlook.com>
2020-01-21 11:04:05 +0700

    Fixed tiny tiny bug...find it ;)

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,6 +1,5 @@
 "use strict"
 
 document.querySelector(".home-blurb-action").addEventListener("click", () => {
-  document.querySelector(".global-hamburger").classList.remove("global-hamburger-state-hidden");
-  document.querySelector(".global-hamburger").classList.add("global-hamburger-state-visible");
+  document.querySelector(".global-menubar-hamburger").click();
 });
